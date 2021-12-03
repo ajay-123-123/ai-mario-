aa45387ed77a6921db5fd5f05476d143b0f34c60
ajay-123-123 <74306143+ajay-123-123@users.noreply.github.com>
2021-12-03 18:53:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+img = "";
+noseX = 0;
+noseY = 0;
+marioX = 325;
+marioY = 325;
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+canvas = createCanvas(1240,336)
+canvas.parent('canvas');
+
+instializeInSetup(mario);
+
+video = createCapture(VIDEO);
+video.size(800,400);
+video.parent('game_console');
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+	background("#D3D3D3")
+	if(noseX < 300)
+	{
+		marioX = marioX - 1;
+	}
+
+	if(noseX > 300)
+	{
+		marioX = marioX + 1;
+	}
+
+	if(noseY < 150)
+	{
+		marioY = marioY - 1;
+	}
+}
+
+function modelLoaded()
+{
+	console.log('modeel Loaded!');
+}
+
+function gotPoses(results)
+{
+	if(results.length > 0)
+	{
+		console.log(results);
+		noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.y;
+	}
+}
+ 
+
+
+
